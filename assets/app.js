b1a8128a6aefb78dc90900c4ac2a95266f55fa50
Jesper15 <230717@edu.rocfriesepoort.nl>
2022-11-23 14:28:19 +0100

    Removing assets

--- assets/app.js
+++ assets/app.js
@@ -6,12 +6,31 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-import './styles/flex-slider.css';
-import './styles/owl.css';
-import './styles/fontawesome.css';
-import './styles/lightbox.css';
-import './styles/templatemo-edu-meeting.css';
+// import './styles/app.css';
+// import './styles/flex-slider.css';
+// import './styles/owl.css';
+// import './styles/fontawesome.css';
+// import './styles/lightbox.css';
+// import './styles/templatemo-edu-meeting.css';
+// import './js/lightbox'
+// import './js/isotope'
+// import './js/tabs'
+// import './js/video'
+// import './js/slick-slider'
+// import './js/custom'
 
-// start the Stimulus application
-import './bootstrap';
+// const $ = require('jquery');
+// // this "modifies" the jquery module: adding behavior to it
+// // the bootstrap module doesn't export/return anything
+// require('bootstrap');
+//
+// // or you can include specific pieces
+// // require('bootstrap/js/dist/tooltip');
+// // require('bootstrap/js/dist/popover');
+//
+// $(document).ready(function() {
+//     $('[data-toggle="popover"]').popover();
+// });
+//
+// // start the Stimulus application
+// import './bootstrap';
