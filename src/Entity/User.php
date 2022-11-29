a938167110388c7fbfdddf5f06d73cb0f6a41e17
Jesper15 <230717@edu.rocfriesepoort.nl>
2022-11-29 14:12:31 +0100

    Finishing login system.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,6 +6,7 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints\Email;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
@@ -15,6 +16,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Email]
     #[ORM\Column(length: 180, unique: true)]
     private ?string $email = null;
 
